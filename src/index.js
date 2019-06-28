8e7909eda6a5be0e97b13a030a029c729ef62d80
IngWilsonBenavides <ing.wilson.benavides@gmail.com>
2019-06-28 15:41:33 -0500

    loader de imagenes y de otros archivos en webpack, se agregan imagenes svg

--- src/index.js
+++ src/index.js
@@ -7,3 +7,12 @@ const profesor = 'Jonathan'
 console.log('Hola mundo Webpack...')
 console.log(`Hola ${profesor}`)
 console.log(suma(9,20))
+
+const d = document,
+	app = d.getElementById('app'),
+	h1 = d.createElement('h1')
+
+h1.textContent = 'Hi again webpack'	
+
+app.appendChild(h1)
+app.appendChild(img)
